90c4be3932758ed5962d08cf74ee586ced83c80e
Gideon <gideonlal@administrators-MacBook-Pro.local>
2020-11-28 11:51:53 -0500

    change api file

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
+import {WEATHER_API_KEY} from "./apiKey.js";
 const api = {
-    key : "4c42671169a9a6d707395b46918d37d8",
+    key : WEATHER_API_KEY,
     currentWeather : `http://api.openweathermap.org/data/2.5/weather?`,
     oneCall : `http://api.openweathermap.org/data/2.5/onecall?`,
     exclude : "&exclude=current,minutely,hourly,alert",
