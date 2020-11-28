d4abb4e556f5c18e099b7dcfb747eea5332fdfc5
glal <61767052+gideonlal@users.noreply.github.com>
2020-11-28 16:48:53 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ sessionStorage.setItem("currentLocation",false);
 
 window.onload=  () => {
     let cityName= JSON.parse(localStorage.getItem("defaultLocation"));
-    if(cityName[0] == null || cityName[0].length == 1){
+    if(localStorage.getItem("defaultLocation")===null){
         fetch.getApi(defaultCity,0,0);
     }
     else{
